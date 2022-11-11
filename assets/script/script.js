6348c7494f0c6165cc97bc7e163edf7bceaa8a76
leozizz <leonardocps2015@gmail.com>
2022-11-11 15:30:52 -0300

    Modificando scripts - refatorando

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,65 +1,27 @@
-const nameInput = document.querySelector("#name")
-const emailInput = document.querySelector("#email")
-const phoneInput = document.querySelector("#phone")
-const messageInput = document.querySelector("#message")
-const submitButton = document.querySelector("#submit")
-const errorMessage = document.querySelectorAll(".error")
+const inputs = document.querySelectorAll(".input")
 
-const inputValues = [
-                    nameInput.value,
-                    emailInput.value,
-                    phoneInput.value,
-                    messageInput.value
-]
+let error = document.querySelectorAll(".error")
 
-const inputs = [
-                        nameInput,
-                        emailInput,
-                        phoneInput,
-                        messageInput
-]
-
-changeInputBorder()
+const submitButton = document.querySelector('#submit')
 
 submitButton.addEventListener("click", function(e) {
+    e.preventDefault()
 
-    checkInputValues()
-    })
+    errorAlert ()
+})
 
-//Check if inputs are empty
-function checkInputValues() {
-    inputValues.forEach(function(item){
-        if (item === ""){
-            errorAlert()
-        }
-    })
-}
-
-//Sets the border color according to the situation.
-//If filled = green. Else = keep the color black.
-function changeInputBorder() {
-    inputs.forEach (function (item){
-        item.addEventListener('input', event => {
-            if (item.value) {
-                item.style.borderColor = "lime"
-            } else {
-                item.style.borderColor = "black"
-                console.log(inputs)
-            }
-        })
-    })
-}
-
-
-//If the inputs are empty displays an error message.
-//The divs in errorMessage[] receive the error text.
 function errorAlert() {
-
     inputs.forEach(function (item) {
-        item.style.borderColor = "#F52E2E"
-    })
+        if (item.value === "") {
+            item.style.borderColor = "red"
+            errorMessage()
+        } else {
+            item.style.borderColor = "green"
+        }
+    })}
 
-    errorMessage.forEach(function (item) { 
-        item.textContent = "campo obrigatório"
+function errorMessage() {
+    error.forEach(function (item) {
+        item.textContent = "Campo obrigatório"
     })
 }
